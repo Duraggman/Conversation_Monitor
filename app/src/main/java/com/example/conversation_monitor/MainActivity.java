67b8b1581dea065b02dac21bb5d6fe7b55be3a36
Duraggman <75789781+Duraggman@users.noreply.github.com>
2024-03-09 01:54:31 +0000

    Commit 4: Start work on getting the deepspeech lib to transcribe in real-time. Also tested out methods. TODO: Continue working on real-time transcribe, Test app on actual device.
    Signed-off-by: Duraggman <75789781+Duraggman@users.noreply.github.com>

--- app/src/main/java/com/example/conversation_monitor/MainActivity.java
+++ app/src/main/java/com/example/conversation_monitor/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.conversation_monitor;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.mozilla.speechlibrary.SpeechResultCallback;
 import com.mozilla.speechlibrary.SpeechService;
 import com.mozilla.speechlibrary.SpeechServiceSettings;
+import com.mozilla.speechlibrary.stt.STTResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button recordButton;
+    TextView rText;
+
+    StringBuilder transcriptionBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +26,61 @@ public class MainActivity extends AppCompatActivity {
         // Initialize DeepSpeech SpeechService with the language model "en-UK"
         SpeechServiceSettings.Builder settingsBuilder = new SpeechServiceSettings.Builder();
         settingsBuilder.withLanguage("en-UK");
-        SpeechService mSpeechService = new SpeechService(this);
+        settingsBuilder.withModelPath("file:///android_asset/deepspeech-0.9.3-models.pbmm");
+        settingsBuilder.withUseDeepSpeech(true);
+        settingsBuilder.withStoreTranscriptions(true);
 
+        SpeechService mSpeechService = new SpeechService(this); // Initialize the SpeechService with the current context
+
+         recordButton = findViewById(R.id.rBtn);
+            rText = findViewById(R.id.rText);
+        recordButton.setOnClickListener(v -> mSpeechService.start(settingsBuilder.build(), mVoiceSearchListener));
     }
+
+
+    SpeechResultCallback mVoiceSearchListener = new SpeechResultCallback() {
+
+        @Override
+        public void onStartListen() {
+            // Handle when the api successfully opened the microphone and started listening
+            recordButton.setText("Listening...");
+        }
+
+        @Override
+        public void onMicActivity(double fftsum) {
+            // Captures the activity from the microphone
+        }
+
+        @Override
+        public void onDecoding() {
+            // Handle when the speech object changes to decoding state
+        }
+
+        @Override
+        public void onSTTResult(@Nullable STTResult result) {
+            // When the api finished processing and returned a hypothesis
+
+            if (result != null) {
+                String transcript = result.mTranscription;
+                transcriptionBuilder.append(transcript);
+
+                // Update UI or perform any actions with the transcription
+                // For example, you can display the transcription in a TextView in real-time.
+                rText.setText(transcriptionBuilder.toString());
+            }
+        }
+
+        @Override
+        public void onNoVoice() {
+            // Handle when the api didn't detect any voice
+        }
+
+        @Override
+        public void onError(@SpeechResultCallback.ErrorType int errorType, @Nullable String error) {
+            // Handle when any error occurred
+
+        }
+    };
+
 
 }
